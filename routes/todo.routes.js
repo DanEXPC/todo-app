f2da73fe02da558a31ecad02c15ac9341bdaf1d7
Danylo <danylosmahliuk@gmail.com>
2021-01-29 19:37:43 +0200

    Router setting

---
+++ routes/todo.routes.js
@@ -0,0 +1,25 @@
+const {Router} = require('express')
+const router = Router()
+
+// Get list of tasks
+router.get('/', (req, res) => {
+    
+})
+
+// Create new task
+router.post('/', (req, res) => {
+
+})
+
+// Change task
+router.put('/:id', (req, res) => {
+
+})
+
+// Delete task
+router.delete('/:id', (req, res) => {
+
+})
+
+
+module.exports = router
